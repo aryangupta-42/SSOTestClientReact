23a2e5e2b365cf07fc7517370cff8356c5dfefe3
Aryan <aryanguptaleo@gmail.com>
2020-05-25 14:02:31 +0530

    Incoporated Store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 // eslint-disable-next-line import/extensions
 import App from './App.jsx';
+import store from './utility/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// import auth from './auth';
+// import alert from './alert';
+// import admin from './admin';
+
+export default combineReducers({
+    // auth,
+    // alert,
+    // admin,
+});
